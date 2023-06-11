55b8bce67bd70639c92458032248d0ceaed207eb
Luigi Moraes <luigimoraessantos@gmail.com>
2023-06-11 16:47:44 -0300

    removed forms to make the core more 'simple'. prettier update.

--- README.md
+++ README.md
@@ -3,22 +3,28 @@
 :construction: Em construção :construction:
 
 ## Descrição
+
 Este framework é um pequeno projeto meu baseado nos frameworks Vuetify e Bootstrap.
 Meu objetivo com este projeto é aprender CSS3 e Javascript mais a fundo.</br>
 
 ## HTML Semântico
+
 O uso de HTML Semântico é necessário para usar o framework.
 
 ## Classes
+
 Há dois tipos de classes: as Component Classes, que devem estar ligadas a uma tag html
 específica, e as Style Classes, classes genéricas que podem estilizar qualquer componente.
 
 ## Tecnologias
-- HTML5
-- CSS3
-- SCSS
-- Javascript
+
+-   HTML5
+-   CSS3
+-   SCSS
+-   Javascript
 
 ## Autor
+
 Luigi Moraes
-  - [Github](https://github.com/luigi-ms)
+
+-   [Github](https://github.com/luigi-ms)
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
-const radioBtns = document.getElementsByClassName('radio');
-const radioGroup = document.getElementsByClassName('radio-group');
+const radioBtns = document.getElementsByClassName("radio");
+const radioGroup = document.getElementsByClassName("radio-group");
 
 let radioGroupsCounter = 0;
 
 /* Add a group name to radio inputs */
-Array.from(radioBtns).forEach(radioEl => {
-  const isInRadioGroup = (radioEl.parentNode.className === "btn-group-item");
+Array.from(radioBtns).forEach((radioEl) => {
+  const isInRadioGroup = radioEl.parentNode.className === "btn-group-item";
 
-  if(isInRadioGroup){
-    Array.from(radioGroup).forEach(fieldset => {
+  if (isInRadioGroup) {
+    Array.from(radioGroup).forEach((fieldset) => {
       let groupName = `group-${radioGroupsCounter}`;
 
-      fieldset.childNodes.forEach(label => {
-        if(label.className === "btn-group-item"){
+      fieldset.childNodes.forEach((label) => {
+        if (label.className === "btn-group-item") {
           label.childNodes[3].name = groupName;
         }
       });
@@ -21,4 +21,3 @@ Array.from(radioBtns).forEach(radioEl => {
     });
   }
 });
-
